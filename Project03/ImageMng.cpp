c2deb238319d7f6656ff78c31c49ecabfc55ef4b
starcatneko <starcatneko@gmail.com>
2018-09-07 02:38:59 +0900

    age

--- Project03/ImageMng.cpp
+++ Project03/ImageMng.cpp
@@ -1,6 +1,19 @@
 #include "ImageMng.h"
+#include "DxLib.h"
 
 
+void ImageMng::Create()
+{
+	s_Instance = new ImageMng();
+}
+
+void ImageMng::Destroy()
+{
+	if (s_Instance)
+		delete s_Instance;
+	s_Instance = nullptr;
+
+}
 
 ImageMng::ImageMng()
 {
--- Project03/ImageMng.h
+++ Project03/ImageMng.h
@@ -1,8 +1,27 @@
 #pragma once
+
+#include <list>
+#include <memory>
+#include <mutex>
+
 class ImageMng
 {
 public:
+	static void Create();
+	static void Destroy();
+
+	static ImageMng *GetInstance()
+	{
+		// mutex call_once ˆê“x‚¾‚¯ŒÄ‚Ô
+		std::call_once(initFlag, Create);
+		Create();
+		return s_Instance;
+	};
+
+private:
 	ImageMng();
 	~ImageMng();
+	static std::once_flag initFlag;
+	static ImageMng *s_Instance;
 };
 
