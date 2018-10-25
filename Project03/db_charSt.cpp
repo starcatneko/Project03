d469a4fc6c4332d4c578f77069ee5586b34bea67
starcatneko <star.cat.neko@gmail.com>
2018-10-25 11:15:23 +0900

    cocoa24

---
+++ Project03/db_charSt.cpp
@@ -0,0 +1,26 @@
+#include "Project03\Exceldb_charSt.h"
+Exceldb_charSt::Exceldb_charSt()
+{
+}
+Exceldb_charSt::~Exceldb_charSt()
+ {
+}
+void Exceldb_charSt::SetDB()
+{ 
+
+//std::vector<std::vector<unsigned int>> chordStateList
+	chordStateList = {
+		{	1	,	0	,	0	,	1	,	0	}, // 	King
+		{	0	,	1	,	1	,	0	,	0	}, // 	Queen
+		{	1	,	0	,	0	,	0	,	1	}, // 	Knight
+		{	0	,	1	,	0	,	0	,	0	}, // 	Bishop
+		{	0	,	0	,	1	,	0	,	0	}, // 	Rook
+	};
+	unitName = {
+		"King",
+		"Queen",
+		"Knight",
+		"Bishop",
+		"Rook"
+	};
+}
